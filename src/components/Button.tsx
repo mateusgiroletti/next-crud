31235b1502a8418e1836ed289bc0df57a3599625
Mateus Giroletti <mateusgiroletti97@gmail.com>
2022-08-06 16:23:29 -0300

    Adicionando componente Button

---
+++ src/components/Button.tsx
@@ -0,0 +1,20 @@
+interface ButtonProps {
+    cor?: 'green' | 'blue' | 'gray'
+    className?: string
+    children: any
+}
+
+export default function Button(props: ButtonProps) {
+    const cor = props.cor ?? 'gray';
+    return (
+        <button className={`
+            bg-gradient-to-r from-${cor}-400 to-${cor}-700
+            text-white 
+            px-4 py-2 
+            rounded-md
+            ${props.className}
+        `}>
+            {props.children}
+        </button>
+    )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import Layout from "../components/Layout";
 import Table from "../components/Table";
+import Button from "../components/Button";
+
 import Client from "../core/Client";
 
 export default function Home() {
@@ -22,7 +24,14 @@ export default function Home() {
     return (
         <div className='flex justify-center items-center h-screen bg-gradient-to-r from-blue-500 to-purple-600 text-white'>
             <Layout title="Cadastro Simples">
-                <Table clients={clients} clientSelected={clientSelected} clientDeleted={clientDeleted}></Table>
+                <div className="flex justify-end">
+                    <Button cor="blue" className="mb-4">Novo Cliente</Button>
+                </div>
+                <Table
+                    clients={clients}
+                    clientSelected={clientSelected}
+                    clientDeleted={clientDeleted}
+                />
             </Layout>
         </div>
     )
